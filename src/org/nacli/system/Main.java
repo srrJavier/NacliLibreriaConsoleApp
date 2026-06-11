70252ef27e4c2679364139b00ab39969714c8e10
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-11 11:46:11 -0600

    001 - se agrego clases Conexion y MenuPrincipal, se edito clase Main

--- src/org/nacli/system/Main.java
+++ src/org/nacli/system/Main.java
@@ -1,21 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/lisuper smash bros.cense-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package org.nacli.system;
 
+package org.nacli.system;
+import org.nacli.view.MenuPrincipal;
 /**
  *
- * @author informatica
+ * @author Javier
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+        MenuPrincipal menu = new MenuPrincipal();
     }
     
 }
---
+++ src/org/nacli/view/MenuPrincipal.java
@@ -0,0 +1,38 @@
+package org.nacli.view;
+import org.nacli.util.Conexion;
+import java.util.Scanner;
+
+public class MenuPrincipal {
+ Scanner leer = new Scanner(System.in);
+    public void iniciar(){
+        int opcion = 0;
+        do {
+            System.out.println("Bienvenido, selecciones una opcion!");
+            System.out.println("1. Modulo Cliente");
+            System.out.println("2. Modulo Autores");
+            System.out.println("3. Modulo Categorias");
+            System.out.println("5. Modulo Editoriales");
+            System.out.println("4. Salir");
+            opcion = Integer.parseInt(leer.nextLine());
+            switch (opcion) {
+                case 1:
+                    System.out.println("Cliente");
+                    break;
+                case 2:
+                    System.out.println("Autor");
+                    break;
+                case 3:
+                    System.out.println("Categoria");
+                    break;
+                case 4:
+                    System.out.println("Adiós Vaquero!");
+                    break;   
+                case 5:
+                    System.out.println("Editoriales");
+                    break;
+                default:
+                    System.out.println("NO existe esta opción");
+            }
+        } while (opcion != 4);
+   }
+}

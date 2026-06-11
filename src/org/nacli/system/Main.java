9eeb15573066b1126c069539837baa865b820008
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-11 10:53:32 -0600

    000 primer commit agregada la estrucutura del proyecto

---
+++ src/org/nacli/system/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/lisuper smash bros.cense-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.nacli.system;
+
+/**
+ *
+ * @author informatica
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+    }
+    
+}

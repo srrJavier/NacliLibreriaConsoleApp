a98a8df0643e866835f53d48281641eff9dd0562
Emilio Pinzon <epinzon-2026037@kinal.edu.gt>
2026-06-19 12:01:06 -0600

    feat(clientes) se agrego la clase DAOimpl al proyecto

--- src/org/nacli/dao/impl/ClientesDAOImpl.java
+++ src/org/nacli/dao/impl/ClientesDAOImpl.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package org.nacli.dao.impl;
 
-/**
- *
- * @author informatica
- */
-public class ClientesDAOImpl {
+package org.nacli.dao.impl;
+import org.nacli.util.Conexion;
+import org.nacli.model.clientes;
+import org.nacli.dao.ClientesDAO;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+
+
+public class ClientesDAOImpl  implements ClientesDAO{
+
+    @Override
+    public boolean insetar(clientes cliente) {
+        return false;
+    }
+
+    @Override
+    public List<clientes> listar() {
+        return null;
+    }
+
+    @Override
+    public clientes buscar(long cui) {
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(clientes cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(long cui) {
+        return false;
+    }
     
 }

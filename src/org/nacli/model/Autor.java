461753d74e7fdbe52fad401d2e786767425960b4
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-19 11:11:24 -0600

    feat(autor) se agrego la clase POJO para Autor #11

--- src/org/nacli/model/Autor.java
+++ src/org/nacli/model/Autor.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.nacli.model;
 
-/**
- *
- * @author informatica
- */
 public class Autor {
+    /*id_autor int primary key auto_increment,
+    nombre_autor varchar(100) not null,
+    apellido_autor varchar(100) not null,
+    nacionalidad varchar(100),
+    biografia text
+    */
+    
+    String idAutor;
+    String nombre;
+    String apellido;
+    String nacionalidad;
+    String biografia;
+    
+    //consultores: asignación de datos, instanciar objetos
+    //vacio
+    public Autor() {
+    }
+    //lleno con parametros
+    public Autor(String idAutor, String nombre, String apellido, String nacionalidad, String biografia) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+        this.biografia = biografia;
+    }
+    //personalizado
+    
+    //getter and setters:
+
+    public String getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(String idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+       //formatear a Mayusculas
+       //formatear a Inicia con Mayuscula
+       String nombreMayuscula = nombre.toUpperCase();
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+    
     
 }

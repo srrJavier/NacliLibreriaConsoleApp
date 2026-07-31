c3f43c4409511eef8e894e8d6cfb17e2627a2b6f
Emilio Pinzon <epinzon-2026037@kinal.edu.gt>
2026-07-31 11:52:15 -0600

    feat(login) se agrego la dashboard y el cleinte view al proyecto

--- src/org/nacli/system/Main.java
+++ src/org/nacli/system/Main.java
@@ -1,13 +1,39 @@
 
 package org.nacli.system;
-import org.nacli.view.MenuPrincipal;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    private static Stage escenarioPrincipal;
+
+ @Override
+public void start(Stage escenarioPrincipal) throws Exception {
+    Main.escenarioPrincipal = escenarioPrincipal;
+
+    FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/org/nacli/view/MenuPrincipal.fxml"));
+
+    Parent root = loader.load();
+
+    Scene scene = new Scene(root);
+    escenarioPrincipal.setTitle("Nacli Librería");
+    escenarioPrincipal.setScene(scene);
+    escenarioPrincipal.show();
+}
+
+    public static void cambiarVista(String fxmlPath) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlPath));
+        Parent root = loader.load();
+        escenarioPrincipal.setScene(new Scene(root));
+    }
 
     public static void main(String[] args) {
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciar();
+        launch(args);
     }
-    
+
 }
---
+++ src/org/nacli/view/ClienteConsoleView.java
@@ -0,0 +1,82 @@
+
+package org.nacli.view;
+
+import java.util.List;
+import java.util.Scanner;
+import org.nacli.model.Cliente;
+
+public class ClienteConsoleView {
+
+    private final Scanner leer = new Scanner(System.in);
+
+    //metodo para mostrar las opcion de este menu
+    public int mostrarMenu() {
+        int opcion = 0;
+        // todo el menu
+        System.out.println("--- GESTION DE CLIENTES ---");
+        System.out.println("-1 CREAR nuevo Cliente ---");
+        System.out.println("-2 LISTAR todos los Clientes ---");
+        System.out.println("-3 BUSCAR Cliente por ID ---");
+        System.out.println("-4 MODIFICAR Cliente ---");
+        System.out.println("-5 ELIMINAR nuevo Cliente ---");
+        System.out.println("-6 REGRESAR a menú PRIMCIPAL ---");
+        System.out.print("SELECCIONE UNA OPCION -->");
+        opcion = Integer.parseInt(leer.nextLine());
+        return opcion;
+    }
+
+    public long solicitarCUI() {
+        System.out.println("Ingrese el CUI del cliente: ");
+        return Long.parseLong(leer.nextLine());
+    }
+
+    //nombreCliente
+    public String solicitarNombreCliente() {
+        String nombre;
+        System.out.println("Ingres el NOMBRE del cliente");
+        nombre = leer.nextLine();
+        return nombre;
+        //return leer.nextLine();
+    }
+
+    //apellidoCliente
+    public String solicitarApellidoCliente() {
+        System.out.println("Ingres el APELLIDO del cliente");
+        return leer.nextLine();
+    }
+
+    //correoEctronico
+    public String solicitarCorreoElectronico() {
+        System.out.println("Ingres el CORREO electrónico del cliente");
+        return leer.nextLine();
+    }
+    
+    //mostrar el detalle de un CLIENTE
+    public void mostrarCliente(Cliente cliente){
+        System.out.println("--- DATOS DEL CLIENTE ---");
+        System.out.println("CUI: " + cliente.getCui());
+        System.out.println("NOMBRE: " + cliente.getNombre());
+        System.out.println("APELLIDO: " + cliente.getApellido());
+        System.out.println("CORREO E: "+ cliente.getCorreoElectronico());
+        System.out.println("---\n");
+    }
+    
+    //mostrar la lista de CLIENTES -- lista de objeto List<T>, ArrayList<Cliente>
+    public void mostrarListaClientes(List<Cliente> clientes){
+        System.out.println("--- LISTA DE CLIENTES ---");
+        //tabla usando la propiedad %-[tamaño de columa]s
+        System.out.printf("%-15s %-10s %-10s %-10s\n", "CUI","NOMBRE","APELLIDO","CORREO");
+        
+        for (Cliente cliente : clientes) {
+            System.out.printf("%-10s %-10s %-10s %-10s\n",
+                    cliente.getCui(), cliente.getNombre(), cliente.getApellido(), cliente.getCorreoElectronico());
+        }
+        System.out.println(" --- fin de clientes ---\n");
+    }
+    
+    //para mostrar mensaje personalizado
+    public void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+}

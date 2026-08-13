24051b7bbc8f2ae1974c39390b8df62e0f1b5b76
EmilioPinzon <epinzon-2026037@kinal.edu.gt>
2026-08-13 07:31:14 -0600

    Revert "Ft/login"

--- src/org/nacli/system/Main.java
+++ src/org/nacli/system/Main.java
@@ -1,39 +1,14 @@
 
 package org.nacli.system;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    private static Stage escenarioPrincipal;
-
- @Override
-public void start(Stage escenarioPrincipal) throws Exception {
-    Main.escenarioPrincipal = escenarioPrincipal;
-
-    FXMLLoader loader = new FXMLLoader(
-            getClass().getResource("/org/nacli/view/MenuPrincipal.fxml"));
-
-    Parent root = loader.load();
-
-    Scene scene = new Scene(root);
-    escenarioPrincipal.setTitle("Nacli Librería");
-    escenarioPrincipal.setScene(scene);
-    escenarioPrincipal.show();
-}
-
-    public static void cambiarVista(String fxmlPath) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlPath));
-        Parent root = loader.load();
-        escenarioPrincipal.setScene(new Scene(root));
-    }
+import org.nacli.view.MenuPrincipal;
+/**
+ *
+ * @author Javier
+ */
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        MenuPrincipal menu = new MenuPrincipal();
     }
-
+    
 }
